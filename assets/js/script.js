ab15e28f1dbe26990e9eb6613af80bbcac402e61
Your Name <felix.acevedo.tech@gmail.com>
2022-03-04 13:54:34 -0500

    date attempts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,10 @@
 // var headerDateEl = document.querySelector('.jumbotron');
 // var todayEl = document.querySelector('#currentDay');
+// const moment = require('moment');
 
 // getTodayDate = function() {
     
-//     var todayFormatEl = moment().format('dddd, MMMM Do');
+//     let todayFormatEl = moment().format('dddd, MMMM Do');
 //     var date = document.createElement("span");
 //     date.textContent = todayFormatEl;
     
@@ -13,13 +14,24 @@
 // };
 // getTodayDate();
 
-let d = new Date();
-let options = {
-    weekday: 'long', 
-    month: 'long',
-    day: 'numeric'
-};
+// let d = new Date();
+// let options = {
+//     weekday: 'long', 
+//     month: 'long',
+//     day: 'numeric'
+// };
 
-console.log(Date.toLocaleTimeString('en-us', options));
+// console.log(Date.toLocaleTimeString('en-us', options));
 
-
+// var curday = function(sp){
+//     today = new Date();
+//     var dd = today.getDate();
+//     var mm = today.getMonth()+1; //As January is 0.
+//     var yyyy = today.getFullYear();
+    
+//     if(dd<10) dd='0'+dd;
+//     if(mm<10) mm='0'+mm;
+//     return (mm+sp+dd+sp+yyyy);
+//     };
+//     console.log(curday('/'));
+//     console.log(curday('-'));
